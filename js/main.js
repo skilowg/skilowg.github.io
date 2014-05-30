c912e1528cd8cfaab437dede4f7cd4ed8053fe5d
skilowg <skilowg@gmail.com>
2014-05-30 12:51:43 -0700

    Refactor Presentation

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$('label').click( function() {
-  if($(this).find('input').is(':checked')) {
-    $(this).find('span').addClass('line'); 
-  } else {
-    $(this).find('span').removeClass('line');
-  }
-});
